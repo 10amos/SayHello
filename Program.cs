a24ecd3920256c024180c307c0090d1ee57e4bae
10amos <62198688+10amos@users.noreply.github.com>
2021-08-03 02:27:16 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Amos!");
+            Console.WriteLine("How are you doing?");
+            Console.WriteLine("I am great, Thanks and you?");
+            Console.WriteLine("I am doing just fine");
+        }
+    }
+}
